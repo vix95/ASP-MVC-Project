262114d1600ff405025b86a3dccf78f6badcf0f2
vix95 <38139321+vix95@users.noreply.github.com>
2020-01-14 18:03:49 +0100

    Initial commit

---
+++ KacikFryzjerski/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KacikFryzjerski.Startup))]
+namespace KacikFryzjerski
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
