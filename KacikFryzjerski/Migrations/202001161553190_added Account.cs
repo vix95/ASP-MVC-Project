3be81d50caaa6ce61c7c406d7b15488bbc7bf033
vix95 <38139321+vix95@users.noreply.github.com>
2020-01-16 16:56:09 +0100

    Added Account

---
+++ KacikFryzjerski/Migrations/202001161553190_added Account.cs
@@ -0,0 +1,16 @@
+namespace KacikFryzjerski.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedAccount : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ KacikFryzjerski/Models/AccountData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KacikFryzjerski.Models
+{
+    public class AccountData
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+        [RegularExpression(@"(\+\d{2})*[\d\s-]+", ErrorMessage = "Błędny format numeru telefonu")]
+        public string Telefon { get; set; }
+
+        [EmailAddress(ErrorMessage = "Błędny format adresu e-mail")]
+        public string Email { get; set; }
+    }
+}
